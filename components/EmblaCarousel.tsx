65e2b88ff7e9bc6396baaffd111074395fe503ec
Travis Martin <tjmartin2003@gmail.com>
2022-03-11 22:31:17 -0600

    Use Vercel.

--- components/EmblaCarousel.tsx
+++ components/EmblaCarousel.tsx
@@ -1,12 +1,12 @@
 import useEmblaCarousel, { UseEmblaCarouselType } from "embla-carousel-react";
-import React, { ReactElement } from "react";
+import React, { ReactChild } from "react";
 import styles from "../styles/embla.module.scss";
 import Autoplay from "embla-carousel-autoplay";
 
 type Properties = {
-  children?: ReactElement[],
+  children?: ReactChild[],
   className?: string,
-  [props: string]: any
+  [props: string]: object | ReactChild | ReactChild[]
 };
 
 export default function EmblaCarousel({ children, className, ...props }: Properties): JSX.Element {
@@ -15,7 +15,7 @@ export default function EmblaCarousel({ children, className, ...props }: Propert
   return (
     <div className={`embla ${styles["embla"]} ${className ?? ""}`} ref={emblaRef} {...props}>
       <div className={`embla__container ${styles["embla__container"]}`}>
-        {(children ?? []).map((item: ReactElement, index: number): JSX.Element => {
+        {(children ?? []).map((item: ReactChild, index: number): JSX.Element => {
           return (
             <div className={`embla__slide ${styles["embla__slide"]}`} key={index} style={{ display: "flex", justifyContent: "center" }}>
               {item}
